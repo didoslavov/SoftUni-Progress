c80804453d05d534a8eb058769073e091e950559
didkoslawow <didkoslawow@gmail.com>
2023-03-08 14:41:36 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Exercise/04. Search in List/search.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   const searchTextElement = document.getElementById('searchText').value;
+   const ulElements = Array.from(document.querySelectorAll('#towns li'));
+   let resultElement = document.getElementById('result').innerHTML;
+   let matches = 0;
+   
+   for (const liElement of ulElements) {
+      if (liElement.textContent.includes(searchTextElement)) {
+         matches++;
+         liElement.style.fontWeight = 'bold';
+         liElement.style.textDecoration = 'underline';
+         document.getElementById('result').textContent = `${matches} matches found`;
+      }
+   }
+
+   document.getElementById('searchText').value = '';
+
 }

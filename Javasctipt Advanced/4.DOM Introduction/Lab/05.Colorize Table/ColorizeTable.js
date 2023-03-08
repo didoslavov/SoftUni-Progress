193adc0bfe8a1e41dbb5d0babe9723e36f0ac422
didkoslawow <didkoslawow@gmail.com>
2023-03-08 09:32:38 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ Javasctipt Advanced/4.DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,11 @@
 function colorize() {
-    // TODO
+  const rowElements = document.querySelectorAll('table tr');
+  let i = 0;
+
+  for (const row of rowElements) {
+    i++;
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'teal';
+    }
+  }
 }

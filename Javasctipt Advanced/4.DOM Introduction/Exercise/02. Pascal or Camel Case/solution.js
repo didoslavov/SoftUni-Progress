08819381542499a1ef764bce7f83e7b0f58001ad
didkoslawow <didkoslawow@gmail.com>
2023-03-08 13:30:14 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO...
+  const text = document.getElementById('text').value;
+  const pattern = document.getElementById('naming-convention').value;
+  const result = document.getElementById('result')
+
+  if (pattern === 'Camel Case') {
+    result.textContent = camelCase(text);
+  } else if (pattern === 'Pascal Case') {
+    result.textContent = pascalCase(text);
+  } else {
+    result.textContent = 'Error!';
+  }
+
+  function camelCase(str) {
+    return str
+      .split(' ')
+      .map((word) => word.toLowerCase())
+      .map((word, index) => {
+        if (index === 0) {
+          return word;
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join('');
+  }
+
+  function pascalCase(string) {
+    return string
+      .split(' ')
+      .map((word) => word.toLowerCase())
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join('');
+  }
 }

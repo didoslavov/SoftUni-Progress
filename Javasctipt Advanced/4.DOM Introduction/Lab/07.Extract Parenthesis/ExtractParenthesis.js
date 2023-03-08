965fd7af145073b4f05332316d08adbee714cc38
didkoslawow <didkoslawow@gmail.com>
2023-03-08 10:08:39 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ Javasctipt Advanced/4.DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,8 @@
 function extract(content) {
+    const contentElement = document.getElementById('content');
+    const text = contentElement.textContent;
 
+    const pattern = /(?<=\().+?(?=\))/gm;
+    const matches = text.match(pattern)
+    return matches.join('; ');
 }

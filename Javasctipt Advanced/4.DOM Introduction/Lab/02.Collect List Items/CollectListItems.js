243826642c9d94c4717053cf749fcad8ad54b65c
didkoslawow <didkoslawow@gmail.com>
2023-03-08 08:44:31 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ Javasctipt Advanced/4.DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    const ulElements = document.getElementById('items')
+    const text = ulElements.textContent;
+    const textAreaElement = document.getElementById('result');
+
+    textAreaElement.textContent = text; 
 }

81fde3f83ad6ef15cd088086dae1b78fcd0662f2
didkoslawow <didkoslawow@gmail.com>
2023-03-08 09:41:00 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ Javasctipt Advanced/4.DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,8 @@
 function sumTable() {
-
+    const priceElements = Array.from(document.querySelectorAll('tr td:nth-of-type(2n)'));
+    const sumElement = priceElements.pop();
+    
+    const totalPrice = priceElements.map(x => Number(x.textContent)).reduce((acc, num) => acc + num, 0);
+    
+    sumElement.textContent = totalPrice;
 }

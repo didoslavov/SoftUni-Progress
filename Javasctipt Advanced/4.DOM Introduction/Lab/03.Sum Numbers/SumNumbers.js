bad6486d3bdff3ff8ded43e45a63c67b71fb8f21
didkoslawow <didkoslawow@gmail.com>
2023-03-08 08:50:43 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
+++ Javasctipt Advanced/4.DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const num1Element = document.getElementById('num1');
+    const num2Element = document.getElementById('num2');
+    const resultElement = document.getElementById('sum');
+
+    const num1 = Number(num1Element.value);
+    const num2 = Number(num2Element.value);
+    const sum = num1 + num2;
+    resultElement.value = sum;
 }

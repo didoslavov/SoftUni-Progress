656f9b150d10d121ed677f0666dad99960e25eae
didkoslawow <didkoslawow@gmail.com>
2023-03-08 13:55:42 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Exercise/03. Accordion/accordion.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttonElement = document.getElementsByClassName('button')[0];
+    const buttonTextContent = document.getElementsByClassName('button')[0];
+    const hiddenElement = document.getElementById('extra');
+
+
+    if (buttonTextContent.innerHTML === 'More') {
+        hiddenElement.style.display = 'block';
+        buttonElement.innerHTML = 'Less';
+    } else {
+        hiddenElement.style.display = 'none';
+        buttonElement.innerHTML = 'More';
+    }
 }

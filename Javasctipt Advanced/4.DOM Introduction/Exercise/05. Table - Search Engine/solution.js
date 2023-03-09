a2a119d3d517418814887fb660660c48d68ca540
didkoslawow <didkoslawow@gmail.com>
2023-03-09 08:15:32 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Exercise/05. Table - Search Engine/solution.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    const searchInputElement = document.getElementById('searchField');
+    const listElements = Array.from(document.querySelectorAll('.container tbody tr'));
+    const searchText = searchInputElement.value;
+    
+    listElements.forEach((x) => {
+      x.className = '';
+    });
+    
+    const filteredElements = listElements.filter(x => {
+      const values = Array.from(x.children);
+      if (values.some(td => td.textContent.includes(searchText))) {
+        x.className = 'select';
+      }
+    })
+    
+   searchInputElement.value = '';
+  }
 }

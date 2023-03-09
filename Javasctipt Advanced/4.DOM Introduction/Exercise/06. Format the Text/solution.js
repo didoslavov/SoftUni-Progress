ced85adf89c2f00dacafc0337561e69333c52a92
didkoslawow <didkoslawow@gmail.com>
2023-03-09 09:23:07 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Exercise/06. Format the Text/solution.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  const outputElement = document.getElementById('output');
+  const inputElement = document.getElementById('input');
+
+  inputElement.value
+    .split('.')
+    .map((x) => x.trim())
+    .filter((x) => x.length !== 0)
+    .reduce((acc, sentance, i) => {
+      if (i % 3 === 0) {
+        acc.push([sentance]);
+      } else {
+        acc[acc.length - 1].push(sentance);
+      }
+      return acc;
+    }, [])
+    .forEach((paragraph) => {
+      const para = document.createElement('p');
+      para.textContent = paragraph + '.';
+      outputElement.appendChild(para);
+    });
 }

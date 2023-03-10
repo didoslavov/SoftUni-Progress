08a30814509020bfdbf21efceff3655a824ee358
didkoslawow <didkoslawow@gmail.com>
2023-03-10 15:36:56 +0200

    solved

--- Javasctipt Advanced/4.DOM Introduction/Exercise/08. Generate Report/report.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,26 @@
 function generateReport() {
-    //TODO
+  const checkBoxElements = document.querySelectorAll('table>thead>tr>th>input');
+  const rowsElements = document.querySelectorAll('tbody>tr');
+  const textArea = document.getElementById('output');
+
+  const output = [];
+
+  Array.from(rowsElements).forEach((row) => {
+    const entry = {};
+    const properties = Array.from(row.getElementsByTagName('td')).map(
+      (e) => e.textContent
+    );
+
+    Array.from(checkBoxElements).forEach((box, i) => {
+      const checkboxName = box.name;
+      
+      if (box.checked) {
+        entry[checkboxName] = properties[i];
+      }
+    });
+
+    output.push(entry);
+  });
+
+  textArea.textContent = JSON.stringify(output, null, 2);
 }

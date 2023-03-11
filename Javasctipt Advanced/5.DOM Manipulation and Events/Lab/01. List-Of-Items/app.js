44193cde4f14e5bbb896204942738837b029bde8
didkoslawow <didkoslawow@gmail.com>
2023-03-11 09:48:20 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Lab/01. List-Of-Items/app.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemElement = document.getElementById('newItemText')
+    const newItem = newItemElement.value;
+    const listElement = document.getElementById('items');
+    const newElement = document.createElement('li');
+
+    newElement.textContent = newItem;
+    listElement.appendChild(newElement);
+
+    newItemElement.value = '';
 }

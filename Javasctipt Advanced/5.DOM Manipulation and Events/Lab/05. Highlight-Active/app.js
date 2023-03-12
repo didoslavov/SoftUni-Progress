9c2922474d1b1210c775a3724d01bcd4f0b5721e
didkoslawow <didkoslawow@gmail.com>
2023-03-12 10:29:24 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+  const boxElements = Array.from(document.querySelectorAll('input'));
+
+  boxElements.forEach((x) => {
+    x.addEventListener('focus', onFocus);
+    x.addEventListener('blur', onBlur);
+  });
+
+  function onFocus(e) {
+    e.target.parentElement.classList.add('focused');
+  }
+
+  function onBlur(e) {
+    e.target.parentElement.classList.remove('focused');
+  }
 }

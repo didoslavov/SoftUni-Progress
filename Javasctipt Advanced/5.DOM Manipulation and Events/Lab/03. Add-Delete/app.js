a82cf1608a140b7efd391643b57928507ceaaef7
didkoslawow <didkoslawow@gmail.com>
2023-03-12 10:01:54 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Lab/03. Add-Delete/app.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,22 @@
 function addItem() {
-    //TODO...
+  const newItemElement = document.getElementById('newItemText');
+  const newItem = newItemElement.value;
+  const listElement = document.getElementById('items');
+
+  const newElement = document.createElement('li');
+  const deleteBtn = document.createElement('a');
+
+  deleteBtn.href = '#';
+  deleteBtn.textContent = '[Delete]';
+  deleteBtn.addEventListener('click', onClick);  
+  newElement.textContent = newItem;
+
+  listElement.appendChild(newElement);
+  newElement.appendChild(deleteBtn)
+  
+  newItemElement.value = '';
+
+  function onClick() {
+    deleteBtn.parentElement.remove()
+  }
 }

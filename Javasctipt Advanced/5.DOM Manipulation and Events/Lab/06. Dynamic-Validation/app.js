e1362dd0409111a25115f5e5045814ebac657bff
didkoslawow <didkoslawow@gmail.com>
2023-03-12 12:09:02 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    const inputElement = document.getElementById('email');
+    inputElement.addEventListener('change', mailValidate);
+    const pattern = /[a-z]+@[a-z]+.[a-z]+/gm;
+
+    function mailValidate() {
+        inputElement.classList.remove('error');
+        const input = inputElement.value;
+
+        if (!input.match(pattern)) {
+            inputElement.classList.add('error');
+        }
+    }
 }
--- Javasctipt Advanced/5.DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+   Array.from(document.querySelectorAll('.add-product')).forEach(x => x.addEventListener('click', addToCart));
+   document.querySelector('.checkout').addEventListener('click', checkout);
+
+   const buttons = Array.from(document.querySelectorAll('button'));
+   const textArea = document.querySelector('textarea');
+   const products = new Set();
+   let total = [];
+   
+   function addToCart(e) {
+      const productName = e.target.parentElement.parentElement.querySelector('.product-title').textContent;
+      const productPrice = e.target.parentElement.parentElement.querySelector('.product-line-price').textContent;
+      
+      textArea.textContent += `Added ${productName} for ${productPrice} to the cart.\n`
+
+      if (!products.has(productName)) products.add(productName);
+      total.push(Number(productPrice));
+
+   }
+
+   function checkout() {
+      const totalPrice = total.reduce((acc, x) => acc + x, 0);
+      textArea.textContent += `You bought ${Array.from(products).join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+      buttons.forEach(b => b.disabled = true);
+   } 
 }

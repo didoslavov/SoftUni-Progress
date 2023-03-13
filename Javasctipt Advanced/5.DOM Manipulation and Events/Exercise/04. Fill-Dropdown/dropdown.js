0e1046558bd82f05c5ab4ca50b3d4e79773fea82
didkoslawow <didkoslawow@gmail.com>
2023-03-13 12:48:53 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const selectElement = document.getElementById('menu');
+
+    const textElement = document.getElementById('newItemText');
+    const valueElement = document.getElementById('newItemValue');
+
+    const newOptionElement = document.createElement('option');
+
+    newOptionElement.textContent = textElement.value;
+    newOptionElement.value = valueElement.value;
+
+    selectElement.appendChild(newOptionElement);
+
+    textElement.value = '';
+    valueElement.value = '';
 }
--- Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,29 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const buttons = Array.from(document.querySelectorAll('button'));
+  const decodedTextArea = buttons[1].previousElementSibling;
+
+  buttons.forEach((b) => b.addEventListener('click', onClick));
+
+  function onClick(e) {
+    if (e.target.textContent === 'Encode and send it') {
+      const text = e.target.previousElementSibling;
+      let encryptedText = '';
+
+      for (let char of text.value) {
+        encryptedText += String.fromCharCode(char.charCodeAt() + 1);
+      }
+
+      decodedTextArea.textContent = encryptedText;
+      text.value = '';
+    } else if (e.target.textContent === 'Decode and read it') {
+      const text = e.target.previousElementSibling;
+      let decriptedText = '';
+
+      for (const char of text.value) {
+        decriptedText += String.fromCharCode(char.charCodeAt() - 1);
+      }
+
+      decodedTextArea.textContent = decriptedText;
+    }
+  }
 }

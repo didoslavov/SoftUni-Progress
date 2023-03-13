99bacad6f8427aefab067918567d72b3b78f5db4
didkoslawow <didkoslawow@gmail.com>
2023-03-13 08:53:51 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/01. Sections/script.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,16 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentDivElement = document.getElementById('content');
+
+   words.forEach(w => {
+      const newDiv = document.createElement('div');
+      const newP = document.createElement('p');
+
+      newP.textContent = w;
+      newP.style.display = 'none';
+      
+      newDiv.appendChild(newP)
+      newDiv.addEventListener('click', () => newDiv.children[0].style.display = 'block')
+
+      contentDivElement.appendChild(newDiv);
+   });
 }

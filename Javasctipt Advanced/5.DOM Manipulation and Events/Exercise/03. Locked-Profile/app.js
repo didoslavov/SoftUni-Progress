f7c779de0cd53f5e580e099e51e57e510ff30a2a
didkoslawow <didkoslawow@gmail.com>
2023-03-13 10:33:13 +0200

    solved

--- Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/03. Locked-Profile/app.js
+++ Javasctipt Advanced/5.DOM Manipulation and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+  Array.from(document.querySelectorAll('button')).forEach((b) =>
+    b.addEventListener('click', showHiddenInfo)
+  );
+
+  function showHiddenInfo(e) {
+    let isLocked = e.target.parentElement.children[2].checked;
+    const hiddenText = e.target.previousElementSibling;
+
+    if (!isLocked) {
+        const button = e.currentTarget;
+
+        if (button.textContent === 'Show more') {
+            hiddenText.style.display = 'block';
+            button.textContent = 'Hide it';
+        } else if (button.textContent === 'Hide it') {
+            hiddenText.style.display = 'none';
+            button.textContent = 'Show more'
+        }
+    }
+  }
 }

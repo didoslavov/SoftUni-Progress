85f15dd7c15893b9c31933ccc1684bc87f604113
didkoslawow <didkoslawow@gmail.com>
2023-03-17 08:41:02 +0200

    solved

--- Javasctipt Advanced/6.Advanced Functions/Exercise/07. Simple Calculator/app.js
+++ Javasctipt Advanced/6.Advanced Functions/Exercise/07. Simple Calculator/app.js
@@ -1,6 +1,27 @@
 function calculator() {
-    // TODO:
+    let selector1;
+    let selector2;
+    let result;
+
+    const calculate = {
+        init(num1, num2, res) {
+            selector1 = document.querySelector(num1);
+            selector2 = document.querySelector(num2);
+            result = document.querySelector(res);
+        },
+        add() {
+            result.value = Number(selector1.value) + Number(selector2.value);
+        },
+        subtract() {
+            result.value = Number(selector1.value) - Number(selector2.value);
+        }
+    }
+    return calculate;
 }
+
+const calculate = calculator();
+calculate.init ('#num1', '#num2', '#result');
+
 
 
 

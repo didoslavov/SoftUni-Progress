192b9cac406156e530d127eb79ff0405c9d218dc
didkoslawow <didkoslawow@gmail.com>
2023-03-17 08:53:50 +0200

    solved

--- Javasctipt Advanced/6.Advanced Functions/Exercise/08. Next Article/script.js
+++ Javasctipt Advanced/6.Advanced Functions/Exercise/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+  return () => {
+    const divElement = document.querySelector('#content');
+    const articleElement = document.createElement('article');
+
+    if (articles.length > 0) {
+    const article = articles.shift();
+    articleElement.innerText = article;
+    divElement.appendChild(articleElement);
+    }
+  };
 }

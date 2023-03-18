e88b50d30e584f0c64e8f9f1bafa0bc0585217a8
didkoslawow <didkoslawow@gmail.com>
2023-03-18 15:35:39 +0200

    solved

---
+++ Javasctipt Advanced/7.Unit Testing and Error Handling/Lab/04sumOfNumbers.test.js
@@ -0,0 +1,16 @@
+const { sum } = require('./04sumOfNumbers');
+const { expect } = require('chai');
+
+describe('Sum of numbers of array', () => {
+    it('Should take array as argument', () => {
+        expect(Array.isArray([1,2,3])).to.be.true;
+    });
+
+    it('Should sum all numbers in the array', () => {
+        expect(sum([1,2,3,4])).to.equal(10, 'Sum is wrong!');
+    });
+
+    it('Should sum all numbers in the array even they\'re strings', () => {
+        expect(sum(['1',2,'3',4])).to.equal(10, 'Need to parse strings to digits!');
+    });
+})

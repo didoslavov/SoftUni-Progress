4436f2686e97c5e063c0c7573d0e7e63ec407311
didkoslawow <didkoslawow@gmail.com>
2023-03-19 12:19:22 +0200

    fixed

--- Javasctipt Advanced/7.Unit Testing and Error Handling/Lab/07addSubtract.test.js
+++ Javasctipt Advanced/7.Unit Testing and Error Handling/Lab/07addSubtract.test.js
@@ -4,28 +4,28 @@ const { createCalculator } = require('./07addSubtract');
 describe('createCalculator method tests', () => {
     let calculator;
 
-    beforeEach(() => calculator = createCalculator())
+    beforeEach(() => calculator = createCalculator());
 
     it('Should return 0 for calculator.get()', () => {
         expect(calculator.get()).to.be.equal(0);
-    })
+    });
     it('Should return 10 after calculator.add(7), calculator.add(3)', () => {
         calculator.add(7);
         calculator.add(3);
         expect(calculator.get()).to.be.equal(10);
-    })
+    });
     it('Should return 4 after calculator.subtract(7), calculator.subtract(3)', () => {
         calculator.subtract(7);
         calculator.subtract(3);
         expect(calculator.get()).to.be.equal(-10);
-    })
+    });
     it('Should return 8.4 after calcalutor.add(10), calculator.subtract(1.6)', () => {
         calculator.add(10);
         calculator.subtract(1.6);
         expect(calculator.get()).to.be.equal(8.4);
-    })
+    });
     it('Should return NaN if calculator.add(string)', () => {
         calculator.add('some string');
         expect(calculator.get()).to.be.NaN;
-    })
-})
+    });
+});

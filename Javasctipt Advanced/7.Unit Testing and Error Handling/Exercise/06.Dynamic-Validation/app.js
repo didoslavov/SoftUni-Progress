71d43d905ca63a421cde6bc8e7896c1cb5018fe5
didkoslawow <didkoslawow@gmail.com>
2023-03-20 08:00:34 +0200

    solved

--- Javasctipt Advanced/7.Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
+++ Javasctipt Advanced/7.Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+  const pattern = /[a-z]+@[a-z]+\.[a-z]+/gm;
+  document.getElementById('email').addEventListener('change', (e) => {
+    const mail = e.target.value;
+
+    if (!pattern.test(mail)) {
+      e.target.classList.add('error');
+    } else {
+        e.target.classList.remove('error');
+    }
+  });
 }

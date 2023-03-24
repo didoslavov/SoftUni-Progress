3ec4306fcffbfcde06f3a69b034dfe3a591ca951
didkoslawow <didkoslawow@gmail.com>
2023-03-24 12:49:58 +0200

    solved

--- Javasctipt Advanced/8.Classes and Attributes/Exercise/11. View-Model/app.js
+++ Javasctipt Advanced/8.Classes and Attributes/Exercise/11. View-Model/app.js
@@ -1,8 +1,28 @@
 class Textbox {
-    //TODO
+  constructor(selector, pattern) {
+    this._elements = document.querySelectorAll(selector);
+    this._invalidSymbols = pattern;
+    Array.from(this.elements).forEach((x) =>
+      x.addEventListener('change', () => (this.value = x.value))
+    );
+  }
+
+  get value() {
+    return this.elements[0].value;
+  }
+  set value(v) {
+    Array.from(this._elements).forEach((x) => (x.value = v));
+  }
+
+  get elements() {
+    return this._elements;
+  }
+
+  isValid() {
+    return !this._invalidSymbols.test(this.value);
+  }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = document.getElementsByClassName('.textbox');
-
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+let textbox = new Textbox('.textbox', /[^a-zA-Z0-9]/);
+let inputs = document.getElementsByClassName('textbox');
+textbox.value = 'Tesst';

3520f882646699395a70c5fe8f848183524ba5ef
didkoslawow <didkoslawow@gmail.com>
2023-04-06 10:27:35 +0300

    solved

--- Javascript Applications/Asynchronous Programming/02.Bus-Schedule/app.js
+++ Javascript Applications/Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,17 +1,48 @@
 function solve() {
+  const info = document.getElementById('info').firstChild;
+  const initUrl = 'http://localhost:3030/jsonstore/bus/schedule/';
 
-    function depart() {
-        console.log('Depart TODO...');
+  const departBtn = document.getElementById('depart');
+  const arriveBtn = document.getElementById('arrive');
+  
+  let nextStop = 'depot';
+  let arriving = '';
+
+  async function depart() {
+    try {
+      const response = await fetch(initUrl + nextStop);
+
+      if (response.status !== 200) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+
+      info.textContent = `Next stop ${data.name}`;
+      arriving = data.name;
+      nextStop = data.next;
+
+      departBtn.disabled = true;
+      arriveBtn.disabled = false;
+    } catch (error) {
+      info.textContent = error;
+
+      departBtn.disabled = true;
+      arriveBtn.disabled = true;
     }
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function arrive() {
+    info.textContent = `Arriving at ${arriving}`;
 
-    return {
-        depart,
-        arrive
-    };
+    arriveBtn.disabled = true;
+    departBtn.disabled = false;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();

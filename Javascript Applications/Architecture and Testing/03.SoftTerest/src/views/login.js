50412a2a7cd96e7df7ea4d8aa89ed77b5120a6ac
didkoslawow <didkoslawow@gmail.com>
2023-04-22 09:07:50 +0300

    login functionality

--- Javascript Applications/Architecture and Testing/03.SoftTerest/src/views/login.js
+++ Javascript Applications/Architecture and Testing/03.SoftTerest/src/views/login.js
@@ -1,8 +1,25 @@
-import { creeateElement } from '../dom.js';
+import { login } from '../api/api.js';
 
 const section = document.getElementById('loginPage');
 section.remove();
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx = null;
 
-export async function showLoginPage(ctx) {
+export async function showLoginPage(ctxTarget) {
+  ctx = ctxTarget;
   ctx.showSection(section);
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const email = formData.get('email').trim();
+  const password = formData.get('password').trim();
+
+  await login(email, password);
+  ctx.goTo('home');
+  ctx.updateNav();
 }

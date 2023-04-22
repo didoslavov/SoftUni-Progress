6008969f073b0333675950a579bc6d818ce2a61d
didkoslawow <didkoslawow@gmail.com>
2023-04-22 09:21:55 +0300

    register functionality

--- Javascript Applications/Architecture and Testing/03.SoftTerest/src/views/register.js
+++ Javascript Applications/Architecture and Testing/03.SoftTerest/src/views/register.js
@@ -1,8 +1,33 @@
-import { creeateElement } from '../dom.js';
+import { register } from '../api/api.js';
 
 const section = document.getElementById('registerPage');
 section.remove();
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx = null;
 
-export async function showRegisterPage(ctx) {
+export async function showRegisterPage(ctxTarget) {
+  ctx = ctxTarget;
   ctx.showSection(section);
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const email = formData.get('email').trim();
+  const password = formData.get('password').trim();
+  const repass = formData.get('repeatPassword').trim();
+
+  if (!email || !password) {
+    return alert('All fields are required !');
+  }
+
+  if (password != repass) {
+    return alert("Passwords don't match !");
+  }
+  await register(email, password);
+  ctx.goTo('home');
+  ctx.updateNav();
 }

eb0787a3b4f3bf33c23e4642a50f5164b389e9be
didkoslawow <didkoslawow@gmail.com>
2023-04-26 15:20:07 +0300

    added details page for a furniture

--- Javascript Applications/Routing/01.Furniture/src/views/details.js
+++ Javascript Applications/Routing/01.Furniture/src/views/details.js
@@ -1,5 +1,44 @@
-import { html } from '../lib.js';
+import { getFurnitureById } from '../api/data.js';
+import { html, until } from '../lib.js';
+import { getUserData } from '../util.js';
+
+const detailsTemplate = (dataPromise) => html` <div class="row space-top">
+    <div class="col-md-12">
+      <h1>Furniture Details</h1>
+    </div>
+  </div>
+  ${until(dataPromise, html`<p>Loading &hellip;</p>`)}
+  </div>`;
+
+const furnitureTemplate = (furniture, userId) => html`<div class="row space-top">
+  <div class="col-md-4">
+    <div class="card text-white bg-primary">
+      <div class="card-body">
+        <img src=${furniture.img} />
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <p>Make: <span>${furniture.make}</span></p>
+    <p>Model: <span>${furniture.model}</span></p>
+    <p>Year: <span>${furniture.year}</span></p>
+    <p>Description: <span>${furniture.description}</span></p>
+    <p>Price: <span>${furniture.price}</span></p>
+    <p>Material: <span>${furniture.material}</span></p>
+    <div>
+      ${userId == furniture._ownerId ? html`<a href="/edit" class="btn btn-info">Edit</a> <a href="/delete" class="btn btn-red">Delete</a>` : null}
+    </div>
+  </div>
+</div>`;
 
 export function detailsPage(ctx) {
-  console.log('Details page', ctx.params.id);
+  const id = ctx.params.id;
+  ctx.render(detailsTemplate(loadFurniture(id)));
+}
+
+async function loadFurniture(id) {
+  const userId = getUserData().id;
+  const furniture = await getFurnitureById(id);
+
+  return furnitureTemplate(furniture, userId);
 }

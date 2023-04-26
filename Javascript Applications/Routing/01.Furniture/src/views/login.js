182c64a77ca36deb5b88c4191eea11f8f8e5851e
didkoslawow <didkoslawow@gmail.com>
2023-04-26 10:21:41 +0300

    create login page

--- Javascript Applications/Routing/01.Furniture/src/views/login.js
+++ Javascript Applications/Routing/01.Furniture/src/views/login.js
@@ -1,5 +1,40 @@
+import { login } from '../api/data.js';
 import { html } from '../lib.js';
 
-export function loginPage() {
-  console.log('Login page');
+const loginTemplate = (onSubmit) => html` <div class="row space-top">
+    <div class="col-md-12">
+      <h1>Login User</h1>
+      <p>Please fill all fields.</p>
+    </div>
+  </div>
+  <form @submit=${onSubmit}>
+    <div class="row space-top">
+      <div class="col-md-4">
+        <div class="form-group">
+          <label class="form-control-label" for="email">Email</label>
+          <input class="form-control" id="email" type="text" name="email" />
+        </div>
+        <div class="form-group">
+          <label class="form-control-label" for="password">Password</label>
+          <input class="form-control" id="password" type="password" name="password" />
+        </div>
+        <input type="submit" class="btn btn-primary" value="Login" />
+      </div>
+    </div>
+  </form>`;
+
+export function loginPage(ctx) {
+  ctx.render(loginTemplate(onSubmit));
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    await login(email, password);
+    e.target.reset();
+    ctx.page.redirect('/');
+  }
 }

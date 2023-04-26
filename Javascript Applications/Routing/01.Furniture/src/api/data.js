af675b0db937143a16dea5c4304b893af5de872f
didkoslawow <didkoslawow@gmail.com>
2023-04-26 09:51:04 +0300

    routing functions

---
+++ Javascript Applications/Routing/01.Furniture/src/api/data.js
@@ -0,0 +1,38 @@
+import * as api from './api.js';
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+const endpoints = {
+  all: '/data/catalog',
+  byId: '/data/catalog/',
+  myFurnitures: (userId) => `http://localhost:3030/data/catalog?where=_ownerId%3D%22${userId}%22`,
+  create: '/data/catalog',
+  edit: '/data/catalog/',
+  delete: '/data/catalog/',
+};
+
+export async function getAllFurniture() {
+  return api.get(endpoints.all);
+}
+
+export async function getFurnitureById(id) {
+  return api.get(endpoints.byId + id);
+}
+
+export async function getMyFurnitures(userId) {
+  return api.get(endpoints.myFurnitures(userId));
+}
+
+export async function createFurniture(data) {
+  return api.post(endpoints.create, data);
+}
+
+export async function editFurniture(id, data) {
+  return api.put(endpoints.edit + id, data);
+}
+
+export async function deleteFurniture(id) {
+  return api.put(endpoints.delete + id);
+}

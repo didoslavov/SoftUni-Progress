6d0aaeaaf73fe30cc8e329a4678b89423577e681
didkoslawow <didkoslawow@gmail.com>
2023-05-06 14:54:21 +0300

    solved

--- Javascript Applications/Exam Prep/JS Applications Retake Exam - 11 April 2023/Eventer/src/api/data.js
+++ Javascript Applications/Exam Prep/JS Applications Retake Exam - 11 April 2023/Eventer/src/api/data.js
@@ -23,3 +23,11 @@ export async function editEvent(event, id) {
 export async function deleteEvent(id) {
   return await api.del('/data/events/' + id);
 }
+
+export async function goToEvent(id) {
+  return await api.post('/data/going', id);
+}
+
+export async function getGoingCount(eventId) {
+  return await api.get(`/data/going?where=eventId%3D%22${eventId}%22&distinct=_ownerId&count`);
+}
--- Javascript Applications/Exam Prep/JS Applications Retake Exam - 11 April 2023/Eventer/src/views/details.js
+++ Javascript Applications/Exam Prep/JS Applications Retake Exam - 11 April 2023/Eventer/src/views/details.js
@@ -1,8 +1,8 @@
 import { html } from '../../lib.js';
 import { getUserData, updateUserNav } from '../../util.js';
-import { deleteEvent, getEventById } from '../api/data.js';
+import { deleteEvent, getEventById, getGoingCount, goToEvent } from '../api/data.js';
 
-const detailsTemplate = (isOwner, isLoggedIn, event, onEdit, onDelete) => html` <section id="details">
+const detailsTemplate = (isOwner, isLoggedIn, event, onEdit, onDelete, onGoing, goingCount) => html` <section id="details">
   <div id="details-wrapper">
     <img id="details-img" src=${event.imageUrl} />
     <p id="details-title">${event.name}</p>
@@ -13,41 +13,44 @@ const detailsTemplate = (isOwner, isLoggedIn, event, onEdit, onDelete) => html` 
         <span>${event.description}</span>
       </div>
     </div>
-
-    <h3>Going: <span id="go">0</span> times.</h3>
-
-    <!--Edit and Delete are only for creator-->
+    <h3>Going: <span id="go">${goingCount}</span> times.</h3>
     <div id="action-buttons">
       ${isOwner
         ? html` <a @click=${onEdit} href="/edit" id="edit-btn">Edit</a>
             <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>`
         : null}
-
       <!--Bonus - Only for logged-in users ( not authors )-->
-      ${isLoggedIn && !isOwner ? html`<a href="" id="go-btn">Going</a>` : null}
+      ${isLoggedIn && !isOwner ? html`<a @click=${onGoing} href="" id="go-btn">Going</a>` : null}
     </div>
   </div>
 </section>`;
 
 export async function detailsPage(ctx) {
+  const id = ctx.params.id;
   const event = await loadEvent();
   const isOwner = getUserData()?.id == event._ownerId;
   const isLoggedIn = getUserData() != null;
+  const goingCount = await getGoingCount(id);
 
-  ctx.render(detailsTemplate(isOwner, isLoggedIn, event, onEdit, onDelete));
+  ctx.render(detailsTemplate(isOwner, isLoggedIn, event, onEdit, onDelete, onGoing, goingCount));
   updateUserNav();
 
   async function loadEvent() {
-    return getEventById(ctx.params.id);
+    return getEventById(id);
   }
 
   function onEdit(e) {
     e.preventDefault();
-    ctx.page.redirect('/edit/' + ctx.params.id);
+    ctx.page.redirect('/edit/' + id);
   }
 
   async function onDelete() {
-    await deleteEvent(ctx.params.id);
+    await deleteEvent(id);
     ctx.page.redirect('/catalog');
+  }
+
+  async function onGoing() {
+    await goToEvent({ id });
+    document.getElementById('go-btn').style.display = 'none';
   }
 }

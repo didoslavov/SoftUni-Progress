990a8fd2f501b3e5f27e5bc5bc3feda5d1d708ba
didkoslawow <didkoslawow@gmail.com>
2023-05-12 13:16:57 +0300

    fixed bug

--- Javasctipt Advanced/4.DOM Introduction/Exercise/04. Search in List/search.js
+++ Javasctipt Advanced/4.DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,18 +1,21 @@
 function search() {
-   const searchTextElement = document.getElementById('searchText').value;
-   const ulElements = Array.from(document.querySelectorAll('#towns li'));
-   let resultElement = document.getElementById('result').innerHTML;
-   let matches = 0;
-   
-   for (const liElement of ulElements) {
-      if (liElement.textContent.includes(searchTextElement)) {
-         matches++;
-         liElement.style.fontWeight = 'bold';
-         liElement.style.textDecoration = 'underline';
-         dresultElement.textContent = `${matches} matches found`;
-      }
-   }
+  const searchTextElement = document.getElementById('searchText').value;
+  const ulElements = Array.from(document.querySelectorAll('#towns li'));
+  let resultElement = document.getElementById('result');
+  let matches = 0;
 
-   document.getElementById('searchText').value = '';
+  if (searchTextElement == '') {
+    return;
+  }
 
+  for (const liElement of ulElements) {
+    if (liElement.textContent.includes(searchTextElement)) {
+      matches++;
+      liElement.style.fontWeight = 'bold';
+      liElement.style.textDecoration = 'underline';
+    }
+  }
+
+  resultElement.textContent = `${matches} matches found`;
+  document.getElementById('searchText').value = '';
 }

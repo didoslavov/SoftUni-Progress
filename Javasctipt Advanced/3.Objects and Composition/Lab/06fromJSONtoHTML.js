7b54f31475e458c9c1e4010866785814a65a7844
didkoslawow <didkoslawow@gmail.com>
2023-05-20 12:34:40 +0300

    fixed bug

--- Javasctipt Advanced/3.Objects and Composition/Lab/06fromJSONtoHTML.js
+++ Javasctipt Advanced/3.Objects and Composition/Lab/06fromJSONtoHTML.js
@@ -1,46 +1,46 @@
 function fromJSONToHTMLTable(json) {
-  const students = JSON.parse(json);
+    const students = JSON.parse(json);
 
-  const result = ['<table>'];
-  result.push(addKey(students));
-  students.forEach((student) => {
-    result.push(addValue(student));
-  });
-  result.push('</table>');
-
-  console.log(result.join('\n'));
-
-  function addKey(arr) {
-    let result = '  <tr>';
-
-    Object.keys(arr[0]).forEach((key) => {
-      result += `<th>${escapeString(key)}</th>`;
+    const result = ['<table>'];
+    result.push(addKey(students));
+    students.forEach((student) => {
+        result.push(addValue(student));
     });
-    result += '</tr>';
+    result.push('</table>');
 
-    return result;
-  }
+    console.log(result.join('\n'));
 
-  function addValue(obj) {
-    let result = '  <tr>';
-    Object.values(obj).forEach((value) => {
-      result += `<td>${escapeString(value)}</td>`;
-    });
-    result += '</tr>';
+    function addKey(arr) {
+        let result = '  <tr>';
 
-    return result;
-  }
+        Object.keys(arr[0]).forEach((key) => {
+            result += `<th>${escapeString(key)}</th>`;
+        });
+        result += '</tr>';
 
-  function escapeString(key) {
-    let entityMap = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#39;',
-    };
-    return key.toString().replace(/[&<>"']/g, (key) => entityMap[key]);
-  }
+        return result;
+    }
+
+    function addValue(obj) {
+        let result = '  <tr>';
+        Object.values(obj).forEach((value) => {
+            result += `<td>${escapeString(value)}</td>`;
+        });
+        result += '</tr>';
+
+        return result;
+    }
+
+    function escapeString(key) {
+        let entityMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            ' ': '&nbsp;',
+        };
+        return key.toString().replace(/[&<> "]/g, (key) => entityMap[key]);
+    }
 }
 
 fromJSONToHTMLTable(`[{"Name":"Stamat",

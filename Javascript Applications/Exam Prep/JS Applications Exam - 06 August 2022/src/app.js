a003d4ad473bdd00b33457980c4826d517a816c6
didkoslawow <didkoslawow@gmail.com>
2023-07-04 13:12:59 +0300

    login page added

--- Javascript Applications/Exam Prep/JS Applications Exam - 06 August 2022/src/app.js
+++ Javascript Applications/Exam Prep/JS Applications Exam - 06 August 2022/src/app.js
@@ -3,6 +3,7 @@ import { getUserData } from './util.js';
 import { showCatalog } from './views/catalog.js';
 import { showDetails } from './views/details.js';
 import { showHome } from './views/home.js';
+import { showLogin } from './views/login.js';
 
 const main = document.querySelector('main');
 
@@ -12,7 +13,7 @@ page('/catalog', showCatalog);
 page('/catalog/:id', showDetails);
 page('/edit/:id', () => console.log('Edi View'));
 page('/create', () => console.log('Create View'));
-page('/login', () => console.log('Login View'));
+page('/login', showLogin);
 page('/register', () => console.log('Register View'));
 
 page.start();

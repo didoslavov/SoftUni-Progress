1b90431b4ba8a417017cd6161d6750349b853e0e
didkoslawow <didkoslawow@gmail.com>
2023-07-04 15:30:40 +0300

    apply for offer added

--- Javascript Applications/Exam Prep/JS Applications Exam - 06 August 2022/src/api/data.js
+++ Javascript Applications/Exam Prep/JS Applications Exam - 06 August 2022/src/api/data.js
@@ -19,3 +19,15 @@ export async function deleteById(id) {
 export async function editOffer(data, id) {
     return put('/data/offers/' + id, data);
 }
+
+export async function apply(offerId) {
+    return post('/data/applications', { offerId });
+}
+
+export async function getAllApplications(offerId) {
+    return get(`/data/applications?where=offerId%3D%22${offerId}%22&distinct=_ownerId&count`);
+}
+
+export async function getHasApplied(offerId, userId) {
+    return get(`/data/applications?where=offerId%3D%22${offerId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
+}
--- Javascript Applications/Exam Prep/JS Applications Exam - 06 August 2022/src/views/details.js
+++ Javascript Applications/Exam Prep/JS Applications Exam - 06 August 2022/src/views/details.js
@@ -1,7 +1,7 @@
-import { deleteById, getById } from '../api/data.js';
+import { apply, deleteById, getAllApplications, getById, getHasApplied } from '../api/data.js';
 import { html, nothing } from '../lib.js';
 
-const detailsTemplate = (offer, isLogged, isOwner, onDelete) => html`<section id="details">
+const detailsTemplate = (offer, isLogged, isOwner, onDelete, total, hasApplied, onApply) => html`<section id="details">
     <div id="details-wrapper">
         <img id="details-img" src=${offer.imageUrl} alt="example1" />
         <p id="details-title">${offer.title}</p>
@@ -17,13 +17,15 @@ const detailsTemplate = (offer, isLogged, isOwner, onDelete) => html`<section id
                 <span>${offer.requirements}</span>
             </div>
         </div>
-        <p>Applications: <strong id="applications">1</strong></p>
+        <p>Applications: <strong id="applications">${total}</strong></p>
         <div id="action-buttons">
             ${isOwner
                 ? html`<a href="/edit/${offer._id}" id="edit-btn">Edit</a>
                       <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a> `
                 : nothing}
-            ${isLogged && !isOwner ? html`<a href="javascript:void(0)" id="apply-btn">Apply</a>` : nothing}
+            ${isLogged && !isOwner && !hasApplied
+                ? html`<a @click=${onApply} href="javascript:void(0)" id="apply-btn">Apply</a>`
+                : nothing}
         </div>
     </div>
 </section>`;
@@ -33,8 +35,9 @@ export async function showDetails(ctx) {
     const offer = await getById(id);
     const isLogged = ctx.user != undefined;
     const isOwner = isLogged && ctx.user._id == offer._ownerId;
+    const [total, hasApplied] = await Promise.all([getAllApplications(id), getHasApplied(id, ctx.user._id)]);
 
-    ctx.render(detailsTemplate(offer, isLogged, isOwner, onDelete));
+    ctx.render(detailsTemplate(offer, isLogged, isOwner, onDelete, total, hasApplied, onApply));
 
     async function onDelete() {
         const choice = confirm('Are you sure you want to delete this offer?');
@@ -44,5 +47,10 @@ export async function showDetails(ctx) {
 
             ctx.page.redirect('/catalog');
         }
+    }
+
+    async function onApply() {
+        await apply(id);
+        ctx.page.redirect('/catalog/' + id);
     }
 }

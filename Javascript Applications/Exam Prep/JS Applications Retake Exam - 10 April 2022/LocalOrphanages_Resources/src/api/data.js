a08e0b65bf687d96809d353d6951d1df6ac8e5de
didkoslawow <didkoslawow@gmail.com>
2023-07-06 14:29:03 +0300

    details page added

--- Javascript Applications/Exam Prep/JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/api/data.js
+++ Javascript Applications/Exam Prep/JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/api/data.js
@@ -1,4 +1,4 @@
-import { get, post } from './api.js';
+import { del, get, post } from './api.js';
 
 export async function getAllPosts() {
     return get('/data/posts?sortBy=_createdOn%20desc');
@@ -10,4 +10,20 @@ export async function getPostById(id) {
 
 export async function createPost(data) {
     return post('/data/posts', data);
+}
+
+export async function deletePost(id) {
+    return del('/data/posts/' + id);
+}
+
+export async function getTotalDonations(postId) {
+    return get(`/data/donations?where=postId%3D%22${postId}%22&distinct=_ownerId&count`);
+}
+
+export async function getHasDonated(postId, userId) {
+    return get(`/data/donations?where=postId%3D%22${postId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
+}
+
+export async function donate(postId) {
+    return post('/data/donations', { postId });
 }

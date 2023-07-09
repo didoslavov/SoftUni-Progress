77eb2fc80b710b4d68c90e50e576e4863144f33e
didkoslawow <didkoslawow@gmail.com>
2023-07-09 13:02:01 +0300

    create page

---
+++ Javascript Applications/Exam Prep/JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/views/create.js
@@ -0,0 +1,31 @@
+import { create } from '../api/data.js';
+import { html } from '../lib.js';
+import { createSubmitHandler } from '../util.js';
+
+const createTemplate = (onCreate) => html`<section id="create-meme">
+    <form @submit=${onCreate} id="create-form">
+        <div class="container">
+            <h1>Create Meme</h1>
+            <label for="title">Title</label>
+            <input id="title" type="text" placeholder="Enter Title" name="title" />
+            <label for="description">Description</label>
+            <textarea id="description" placeholder="Enter Description" name="description"></textarea>
+            <label for="imageUrl">Meme Image</label>
+            <input id="imageUrl" type="text" placeholder="Enter meme ImageUrl" name="imageUrl" />
+            <input type="submit" class="registerbtn button" value="Create Meme" />
+        </div>
+    </form>
+</section>`;
+
+export function showCreate(ctx) {
+    ctx.render(createTemplate(createSubmitHandler(onCreate)));
+
+    async function onCreate({ title, description, imageUrl }) {
+        if ([title, description, imageUrl].some((x) => x == '')) {
+            return alert('All fields are required');
+        }
+
+        await create({ title, description, imageUrl });
+        ctx.page.redirect('/catalog');
+    }
+}
--- Javascript Applications/Exam Prep/JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/views/details.js
+++ Javascript Applications/Exam Prep/JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/views/details.js
@@ -1,7 +1,7 @@
-import { getById } from '../api/data.js';
+import { deleteById, getById } from '../api/data.js';
 import { html, nothing } from '../lib.js';
 
-const detailsTemplate = (meme, isOwner) => html`<section id="meme-details">
+const detailsTemplate = (meme, isOwner, onDelete) => html`<section id="meme-details">
     <h1>Meme Title: ${meme.title}</h1>
     <div class="meme-details">
         <div class="meme-img">
@@ -11,7 +11,8 @@ const detailsTemplate = (meme, isOwner) => html`<section id="meme-details">
             <h2>Meme Description</h2>
             <p>${meme.description}</p>
             ${isOwner
-                ? html`<a class="button warning" href="/edit/${meme._id}">Edit</a> <button class="button danger">Delete</button>`
+                ? html`<a class="button warning" href="/edit/${meme._id}">Edit</a>
+                      <button @click=${onDelete} class="button danger">Delete</button>`
                 : nothing}
         </div>
     </div>
@@ -21,5 +22,14 @@ export async function showDetails(ctx) {
     const id = ctx.params.id;
     const meme = await getById(id);
     const isOwner = ctx.user?._id == meme._ownerId;
-    ctx.render(detailsTemplate(meme, isOwner));
+    ctx.render(detailsTemplate(meme, isOwner, onDelete));
+
+    async function onDelete() {
+        const choice = confirm('Are you sure?');
+
+        if (choice) {
+            await deleteById(id);
+            ctx.page.redirect('/catalog');
+        }
+    }
 }

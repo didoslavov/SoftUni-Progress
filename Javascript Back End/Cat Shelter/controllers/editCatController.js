355212df2beae1d3487785807344de2314687679
didkoslawow <didkoslawow@gmail.com>
2023-08-13 15:48:31 +0300

    console.log removed

--- Javascript Back End/Cat Shelter/controllers/editCatController.js
+++ Javascript Back End/Cat Shelter/controllers/editCatController.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     const cat = getCatById(id);
-    console.log(cat);
+
     res.render('editCat', { cat });
 });
 

170017977c30af3598b40b88a1d6d4aee4a18fa7
didkoslawow <didkoslawow@gmail.com>
2023-08-29 11:32:53 +0300

    validation added for edit cube

--- Javascript Back End/Cubicle/controllers/editController.js
+++ Javascript Back End/Cubicle/controllers/editController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 const { getCubeById, editCube } = require('../services/cubeService.js');
 const { getSelectedOption } = require('./utils/utils.js');
 
@@ -31,27 +32,37 @@ editController.get('/:id', async (req, res) => {
     }
 });
 
-editController.post('/:id', async (req, res) => {
-    const id = req.params.id;
-    const cube = req.body;
-    const options = getSelectedOption(cube.difficultyLevel);
+editController.post(
+    '/:id',
+    body(['name', 'description', 'imageUrl']).trim().notEmpty().withMessage('All fields are required!'),
+    async (req, res) => {
+        const id = req.params.id;
+        const cube = await getCubeById(id);
+        const options = getSelectedOption(cube.difficultyLevel);
+        const { errors } = validationResult(req);
 
-    try {
-        if (!req.user || cube.ownerId != req.user._id) {
-            throw new Error("You're not creator of this cube!");
+        try {
+            if (!req.user || cube.ownerId != req.user._id) {
+                errors.push({ msg: "You're not creator of this cube!" });
+                throw errors;
+            }
+
+            if (errors.length > 0) {
+                throw errors;
+            }
+
+            await editCube(id, cube);
+            res.redirect('/details/' + id);
+        } catch (error) {
+            res.render('edit', {
+                id,
+                options,
+                cube,
+                title: 'Edit',
+                error: error.map((e) => e.msg),
+            });
         }
-
-        await editCube(id, cube);
-        res.redirect('/details/' + id);
-    } catch (error) {
-        res.render('edit', {
-            id,
-            options,
-            cube,
-            title: 'Edit',
-            error: error.message.split(','),
-        });
     }
-});
+);
 
 module.exports = editController;

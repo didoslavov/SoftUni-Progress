c53a0aa50541b23f0ef112c075108aaf83fed9e7
didoslavov <didkoslawow@gmail.com>
2024-01-09 18:05:28 +0200

    extracted popup component

--- Angular/Components/todo app assignment/todo-app/src/app/app.component.ts
+++ Angular/Components/todo app assignment/todo-app/src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { PopupComponent } from './popup/popup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, TodoListComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    TodoListComponent,
+    PopupComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ Angular/Components/todo app assignment/todo-app/src/app/popup/popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './popup.component.html',
+  styleUrl: './popup.component.css'
+})
+export class PopupComponent {
+
+}

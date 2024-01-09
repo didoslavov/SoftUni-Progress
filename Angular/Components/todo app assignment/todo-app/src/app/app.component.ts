7d4984b8da40883260c36f05d15056ec688c3d37
didoslavov <didkoslawow@gmail.com>
2024-01-09 18:33:46 +0200

    extracted button component

--- Angular/Components/todo app assignment/todo-app/src/app/app.component.ts
+++ Angular/Components/todo app assignment/todo-app/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { PopupComponent } from './popup/popup.component';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { PopupComponent } from './popup/popup.component';
     HeaderComponent,
     TodoListComponent,
     PopupComponent,
+    ButtonComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',

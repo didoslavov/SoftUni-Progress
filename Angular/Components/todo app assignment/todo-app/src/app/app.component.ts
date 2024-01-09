10a5bbb064a0ae0b4ae91bdc39bbfa0eb025b8f2
didoslavov <didkoslawow@gmail.com>
2024-01-09 17:46:24 +0200

    init angular project todo-app

---
+++ Angular/Components/todo app assignment/todo-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'todo-app';
+}

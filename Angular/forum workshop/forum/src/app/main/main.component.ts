b3f6b70b9ecf31acd2d4848240cec6d4ff2c51c2
didoslavov <didkoslawow@gmail.com>
2024-01-12 04:36:35 +0200

    fetching themes on main component init

--- Angular/forum workshop/forum/src/app/main/main.component.ts
+++ Angular/forum workshop/forum/src/app/main/main.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../types/Post';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  themes: Theme[] = [];
+  constructor(private api: ApiService) {}
 
+  ngOnInit(): void {
+    this.api.getAll().subscribe({
+      next: (themes) => {
+        this.themes = themes;
+      },
+    });
+  }
 }

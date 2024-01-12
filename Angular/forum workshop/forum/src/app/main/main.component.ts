fa38cf8f2a2f325e3dbc5e5721998e9feedfd972
didoslavov <didkoslawow@gmail.com>
2024-01-12 11:49:25 +0200

    renamed an interface

--- Angular/forum workshop/forum/src/app/main/main.component.ts
+++ Angular/forum workshop/forum/src/app/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { Theme } from '../types/Post';
+import { Theme } from '../types/Theme';
 
 @Component({
   selector: 'app-main',

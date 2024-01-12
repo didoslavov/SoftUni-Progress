e058a142039be1e2cb60eb129ba04eaf05185ef3
didoslavov <didkoslawow@gmail.com>
2024-01-12 03:46:25 +0200

    main component added

--- Angular/forum workshop/forum/src/app/app.component.ts
+++ Angular/forum workshop/forum/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

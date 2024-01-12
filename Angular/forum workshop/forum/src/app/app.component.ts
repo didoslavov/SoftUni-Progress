9162953d262f4c5aa7c4933da5549b8ef33236be
didoslavov <didkoslawow@gmail.com>
2024-01-12 20:04:11 +0200

    resources for next workshop added

--- Angular/forum workshop/forum/src/app/app.component.ts
+++ Angular/forum workshop/forum/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
 import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- Angular/forum workshop/forum/src/app/core/header/header.component.ts
+++ Angular/forum workshop/forum/src/app/core/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}

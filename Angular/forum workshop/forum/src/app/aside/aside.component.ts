9d251d673be25855d4155a170576fecd1872efd1
didoslavov <didkoslawow@gmail.com>
2024-01-12 11:50:17 +0200

    fetching latest posts and display it in the template

--- Angular/forum workshop/forum/src/app/aside/aside.component.ts
+++ Angular/forum workshop/forum/src/app/aside/aside.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../types/Post';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
   imports: [],
   templateUrl: './aside.component.html',
-  styleUrl: './aside.component.css'
+  styleUrl: './aside.component.css',
 })
-export class AsideComponent {
+export class AsideComponent implements OnInit {
+  posts: Post[] = [];
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getLatest().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+        console.log(this.posts);
+      },
+    });
+  }
 }

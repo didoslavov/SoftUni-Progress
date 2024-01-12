1dcd63144cefe1201b8bdb4f1db86e5469509521
didoslavov <didkoslawow@gmail.com>
2024-01-12 03:49:47 +0200

    added aside component

--- Angular/forum workshop/forum/src/app/app.component.ts
+++ Angular/forum workshop/forum/src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MainComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ Angular/forum workshop/forum/src/app/aside/aside.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  standalone: true,
+  imports: [],
+  templateUrl: './aside.component.html',
+  styleUrl: './aside.component.css'
+})
+export class AsideComponent {
+
+}

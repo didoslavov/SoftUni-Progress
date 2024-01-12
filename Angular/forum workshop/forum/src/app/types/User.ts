01af1125444a73701849910d5128829df115b36f
didoslavov <didkoslawow@gmail.com>
2024-01-12 11:47:13 +0200

    exported User interface to reuse it

---
+++ Angular/forum workshop/forum/src/app/types/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  _id: string;
+  themes: string[];
+  posts: string[];
+  tel: string;
+  email: string;
+  username: string;
+  password: string;
+  created_at: string;
+  updatedAt: string;
+}

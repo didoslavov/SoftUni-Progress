fb013b29c1bfc995d1b30bc80182abd8dcc4ef33
didoslavov <didkoslawow@gmail.com>
2024-01-12 03:41:41 +0200

    added header component and styles for it

--- Angular/forum workshop/forum/src/app/app.component.ts
+++ Angular/forum workshop/forum/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'forum';

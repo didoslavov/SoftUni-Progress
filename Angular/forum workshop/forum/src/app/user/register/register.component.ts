863de3c528ead9d2654e39bf4071980a3ea14bfe
didoslavov <didkoslawow@gmail.com>
2024-01-14 01:51:58 +0200

    added validations for register form

--- Angular/forum workshop/forum/src/app/user/register/register.component.ts
+++ Angular/forum workshop/forum/src/app/user/register/register.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { DEFAULT_EMAIL_DOMAINS } from '../../shared/constants';
+import { AppEmailDirective } from '../../shared/validators/app-email.directive';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule, AppEmailDirective],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  appEmailDomains = DEFAULT_EMAIL_DOMAINS;
+
+  onRegister(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+  }
+}

6710ab4049443b84556b888867f8f9486fda3332
didoslavov <didkoslawow@gmail.com>
2024-01-13 02:58:04 +0200

    refactored header and login component to get correct isLoggedIn data from service

--- Angular/forum workshop/forum/src/app/core/header/header.component.ts
+++ Angular/forum workshop/forum/src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private userService: UserService, private router: Router) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLoggedIn;
+  }
+
+  onLogout() {
+    this.userService.logout();
+    this.router.navigate(['/']);
+  }
+}
--- Angular/forum workshop/forum/src/app/user/login/login.component.ts
+++ Angular/forum workshop/forum/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +9,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
+
+  onLogin(email: string, password: string) {
+    this.userService.login();
+    this.router.navigate(['/']);
+  }
+}

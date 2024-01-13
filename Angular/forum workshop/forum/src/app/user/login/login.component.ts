3681d86e52a47c451af17353de3095c7986ab8cb
didoslavov <didkoslawow@gmail.com>
2024-01-14 01:13:04 +0200

    added validations to login form

--- Angular/forum workshop/forum/src/app/user/login/login.component.ts
+++ Angular/forum workshop/forum/src/app/user/login/login.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from '../../shared/constants';
+import { AppEmailDirective } from '../../shared/validators/app-email.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule, AppEmailDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  appEmailDomains = DEFAULT_EMAIL_DOMAINS;
+
   constructor(private userService: UserService, private router: Router) {}
 
-  onLogin(email: string, password: string) {
+  onLogin(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
     this.userService.login();
     this.router.navigate(['/']);
   }

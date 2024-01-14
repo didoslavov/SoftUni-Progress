777bd18473c254c0d7fc0f9ab13affcce97a24d5
didoslavov <didkoslawow@gmail.com>
2024-01-14 03:30:59 +0200

    populate profile component with dynamic data for users

--- Angular/forum workshop/forum/src/app/user/profile/profile.component.ts
+++ Angular/forum workshop/forum/src/app/user/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +10,26 @@ import { Router } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
-  constructor(private router: Router) {}
+export class ProfileComponent implements OnInit {
+  user: User | undefined = {
+    _id: '',
+    themes: [],
+    posts: [],
+    tel: '',
+    email: '',
+    username: '',
+    password: '',
+    created_at: '',
+    updatedAt: '',
+    __v: 0,
+  };
+  constructor(private userService: UserService, private router: Router) {}
 
-  onEditClickHanlder() {
+  ngOnInit(): void {
+    this.user = this.userService.user;
+  }
+
+  onEditClickHanlder(): void {
     this.router.navigate(['/profile-edit']);
   }
 }

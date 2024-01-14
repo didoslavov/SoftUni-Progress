c295f808de759d443096afe278b601966640989e
didoslavov <didkoslawow@gmail.com>
2024-01-14 04:22:30 +0200

    fixed unused code

--- Angular/forum workshop/forum/src/app/core/header/header.component.ts
+++ Angular/forum workshop/forum/src/app/core/header/header.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../user/user.service';
-import { User } from '../../types/user';
 
 @Component({
   selector: 'app-header',
@@ -10,28 +9,15 @@ import { User } from '../../types/user';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent implements OnInit {
-  user: User | undefined = {
-    _id: '',
-    themes: [],
-    posts: [],
-    tel: '',
-    email: '',
-    username: '',
-    password: '',
-    created_at: '',
-    updatedAt: '',
-    __v: 0,
-  };
-
+export class HeaderComponent {
   constructor(private userService: UserService, private router: Router) {}
 
   get isLoggedIn(): boolean {
     return this.userService.isLoggedIn;
   }
 
-  ngOnInit(): void {
-    this.user = this.userService.user;
+  get username(): string | undefined {
+    return this.userService.user?.username;
   }
 
   onLogout(): void {

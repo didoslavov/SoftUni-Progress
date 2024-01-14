6b7a36aa441f8ebd549b7975e08952c77027860c
didoslavov <didkoslawow@gmail.com>
2024-01-14 03:30:23 +0200

    navigate to profile component on cancel click

--- Angular/forum workshop/forum/src/app/user/profile-edit/profile-edit.component.ts
+++ Angular/forum workshop/forum/src/app/user/profile-edit/profile-edit.component.ts
@@ -1,10 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppEmailDirective } from '../../shared/validators/app-email.directive';
+import { DEFAULT_EMAIL_DOMAINS } from '../../shared/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, AppEmailDirective],
   templateUrl: './profile-edit.component.html',
   styleUrl: '../profile/profile.component.css',
 })
-export class ProfileEditComponent {}
+export class ProfileEditComponent {
+  appEmailDomains = DEFAULT_EMAIL_DOMAINS;
+
+  constructor(private router: Router) {}
+
+  onCancel() {
+    this.router.navigate(['/profile']);
+  }
+}

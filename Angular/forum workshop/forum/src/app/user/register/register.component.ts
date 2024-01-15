515f0e008235eccac430d79512b715a2fef1c0ec
didoslavov <didkoslawow@gmail.com>
2024-01-15 12:50:52 +0200

    added register functionality

--- Angular/forum workshop/forum/src/app/user/register/register.component.ts
+++ Angular/forum workshop/forum/src/app/user/register/register.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { DEFAULT_EMAIL_DOMAINS } from '../../shared/constants';
 import { AppEmailDirective } from '../../shared/validators/app-email.directive';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -15,9 +16,23 @@ import { AppEmailDirective } from '../../shared/validators/app-email.directive';
 export class RegisterComponent {
   appEmailDomains = DEFAULT_EMAIL_DOMAINS;
 
+  constructor(private userService: UserService, private router: Router) {}
+
   onRegister(form: NgForm): void {
     if (form.invalid) {
       return;
     }
+
+    const { username, email, tel, password, rePassword } = form.value as {
+      username: string;
+      email: string;
+      tel: string;
+      password: string;
+      rePassword: string;
+    };
+
+    this.userService
+      .register(username, email, password, rePassword, tel)
+      .subscribe((): Promise<boolean> => this.router.navigate(['/']));
   }
 }
